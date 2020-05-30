436d9e2c68090bf0aa9d2e0913778a09f37a6448
Ishan Manchanda <ishanmanchanda70@gmail.com>
2020-05-30 23:10:30 +0530

    Some vcs stuff

--- src/globals.py
+++ src/globals.py
@@ -4,7 +4,15 @@ import os
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 networks_dir = os.path.join(root_dir, 'networks')
+if not os.path.isdir(networks_dir):
+	os.mkdir(networks_dir)
+
 # Directory for the current run
 current_dir = os.path.join(networks_dir, 'current')
+if not os.path.isdir(current_dir):
+	os.mkdir(current_dir)
+
 # Directory for previous runs
 archive_dir = os.path.join(networks_dir, 'archive')
+if not os.path.isdir(archive_dir):
+	os.mkdir(archive_dir)
